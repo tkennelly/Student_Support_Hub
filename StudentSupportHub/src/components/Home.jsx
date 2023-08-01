170ffc303ffd512238b42eec13f374bfbc6a24fd
tkennelly <theresa.a.kennelly@gmail.com>
2023-08-01 15:04:23 -0400

    home styled

--- StudentSupportHub/src/components/Home.jsx
+++ StudentSupportHub/src/components/Home.jsx
@@ -1,10 +1,22 @@
+import Container from 'react-bootstrap/Container'
+import "../App.css"
+
+
 
 const Home = () => {
     return (
+        <Container>
         <div>
+            <br/>
             <h2> Welcome Caseworker! </h2>
-            <h3> Welcome to the Student Support Hub. Click on a student to see their information or add a student to the database. </h3>
+            <br/>
+            <h3> Welcome to the Student Support Hub. </h3>
+            <br/>
+            <h3> Click on a student to see their information or add a student to the database. </h3>
+            <br/>
+            <p className="desc-p"> this is all the information about the app and why it is useful bdsakjfbk fskjfb fdbksj fbkdjs dbskfj sdfbkb sfbdkfj sdkfjb sdkbfjb dsfbksjdbf dsfbksjdbf bkjb dkfsb  dkfjb fsbkdjb bkjbsfd dkbds sdkfhb sdfbkjbds kdjfbkbsdfkjb! ksdjb dksjfb dkjsbf sbkfjb lajns kjbh d fkhbjkjd kdbhfskndflj kbjbfd. </p>
          </div>
+        </Container> 
     )
 }
 
