f367e5712c0fa3b4f7c92e98971d3d5a77320521
tkennelly <theresa.a.kennelly@gmail.com>
2023-08-01 15:07:42 -0400

    student list styled

--- StudentSupportHub/src/components/Caseworker/StudentList.jsx
+++ StudentSupportHub/src/components/Caseworker/StudentList.jsx
@@ -20,11 +20,13 @@ const StudentList = ({allStudents, setAllStudents}) => {
    
     return allStudents ? (
       <div className="student-list">
+        <h1> Students </h1>
+        <br/>
           {
               allStudents.map((student, key) => (
                   <div key={key} onClick={()=>showStudent(student.first_name)} className="student">
-                      <h2>{student.first_name} {student.last_name}</h2>
-                      
+                      <h2 className='student-name'>{student.first_name} {student.last_name}</h2>
+                      <br/>
                   </div>
               ))
           }
